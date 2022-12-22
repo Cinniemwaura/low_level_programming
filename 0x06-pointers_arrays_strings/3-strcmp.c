2cdf372b6a78e1faac18471bbbb055dc9fde8127
Cinniemwaura <cynthiamwaura@gmail.com>
2022-12-22 11:42:11 -0800

    omg

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  *         If str1 == str2, 0.
  *         If str1 > str2, the positive difference
  */
-int _strcmp(char *s1, char *s2) 
+int _strcmp(char *s1, char *s2)
 {
 while (*s1 && *s2 && *s1 == *s2)
 {
